8aefff5883019112d501e96019a64097b120150f
tecnicoemerson <emersonrodrigues5578@gmail.com>
2026-06-17 10:52:42 -0300

    Selecionando candidatos

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,12 +1,34 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        System.out.println("Processo Seletivo");
-        analisarCandidato(1900.0);
-        analisarCandidato(2000.0);
-        analisarCandidato(2200.0);
+        selecaoCandidatos();
     }
+    static void selecaoCandidatos(){
+        String []  candidatos = {"FELIPE", "EMERSON", "JULIA", "PAULO", "AUGUSTO","MONICA", "FABRICIO"};
+
+        int candidosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+        while(candidosSelecionados < 5 && candidatoAtual < candidatos.length){
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " solicitou este valor de salario " + salarioPretendido);
+            if (salarioBase >= salarioPretendido){
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+                candidosSelecionados++;
+           }
+            candidatoAtual++;
+        }
+    }
+
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800,2200);
+    }
+
     static void analisarCandidato(double salarioPretendido){
         double salarioBase = 2000.0;
         if (salarioBase > salarioPretendido) {
