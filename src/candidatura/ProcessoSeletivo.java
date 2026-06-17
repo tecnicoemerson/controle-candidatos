8dcfe5cceacc2890597c1fdc432c88de87c824a0
tecnicoemerson <emersonrodrigues5578@gmail.com>
2026-06-17 10:33:46 -0300

    Analisando candidatos

---
+++ src/candidatura/ProcessoSeletivo.java
@@ -0,0 +1,20 @@
+package candidatura;
+
+public class ProcessoSeletivo {
+    public static void main(String[] args) {
+        System.out.println("Processo Seletivo");
+        analisarCandidato(1900.0);
+        analisarCandidato(2000.0);
+        analisarCandidato(2200.0);
+    }
+    static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido) {
+            System.out.println("Ligar para o candidato");
+        }else if(salarioBase == salarioPretendido){
+            System.out.println("Ligar para o candidato com contra proposta");
+        }else {
+            System.out.println("Aguardando o resultado do demais candidatos");
+        }
+    }
+}
