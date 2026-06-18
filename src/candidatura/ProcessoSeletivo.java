5c03645801aaa852cec70fd7413465f7624bc8b9
tecnicoemerson <emersonrodrigues5578@gmail.com>
2026-06-18 10:33:19 -0300

    Fazendo ligacoes para cada candidato selecionado

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,11 +1,40 @@
 package candidatura;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        imprimirSelecionados();
+        String[] candidatos = {"FELIPE", "EMERSON", "JULIA", "PAULO", "AUGUSTO"};
+        for (String candidato: candidatos){
+            entrandoEmContato(candidato);
+        }
     }
+
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizadas = 1;
+        boolean continuaTentando = true;
+        boolean atendeu = false;
+        do{
+            atendeu = atender();
+            continuaTentando =! atendeu;
+            if (continuaTentando){
+                tentativasRealizadas++;
+            }else{
+                System.out.println("Contato realizado com sucesso");
+            }
+
+        }while (continuaTentando && tentativasRealizadas < 3);
+        if (atendeu){
+            System.out.println("Conseguimos contato com " + candidato + " na " + tentativasRealizadas + " tentativa");
+        }else {
+            System.out.println("Nao conseguimos contato com o " + candidato + ", numero maximo tentativas " + tentativasRealizadas + " realizadas");
+        }
+    }
+
+     static boolean atender(){
+        return new Random().nextInt(3)==1;
+     }
 
     static void imprimirSelecionados(){
         String[] candidatos = {"FELIPE", "EMERSON", "JULIA", "PAULO", "AUGUSTO"};
